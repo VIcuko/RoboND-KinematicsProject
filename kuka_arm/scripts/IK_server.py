99092c1b77def894edae355e139b8e36b1c84fa8
Vicuko <victorc.fernandez@gmail.com>
2017-10-19 12:51:22 +0200

    refactor individual transformation matrices

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -56,50 +56,17 @@ def handle_calculate_IK(req):
             return DH_Matrix
 
     # Create individual transformation matrices
+	
+        T0_1 = DH_T_Matrix(q1, alpha0, d1, a0).subs(s)
+        T1_2 = DH_T_Matrix(q2, alpha1, d2, a1).subs(s)
+        T2_3 = DH_T_Matrix(q3, alpha2, d3, a2).subs(s)
+        T3_4 = DH_T_Matrix(q4, alpha3, d4, a3).subs(s)
+        T4_5 = DH_T_Matrix(q5, alpha4, d5, a4).subs(s)
+        T5_6 = DH_T_Matrix(q6, alpha5, d6, a5).subs(s)
+        T6_G = DH_T_Matrix(q7, alpha6, d7, a6).subs(s)
+	
     # Extract rotation matrices from the transformation matrices
-	
-        T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
-                       [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
-                       [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
-                       [                   0,                   0,            0,               1]])
-        T0_1 = T0_1.subs(s)
-
-        T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
-                       [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
-                       [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
-                       [                   0,                   0,            0,               1]])
-        T1_2 = T1_2.subs(s)
-        
-        T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
-                       [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
-                       [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
-                       [                   0,                   0,            0,               1]])
-        T2_3 = T2_3.subs(s)
-
-        T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
-                       [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
-                       [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
-                       [                   0,                   0,            0,               1]])
-        T3_4 = T3_4.subs(s)
-
-        T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
-                       [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
-                       [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
-                       [                   0,                   0,            0,               1]])
-        T4_5 = T4_5.subs(s)
-
-        T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
-                       [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
-                       [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
-                       [                   0,                   0,            0,               1]])
-        T5_6 = T5_6.subs(s)
-
-        T6_G = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
-                       [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
-                       [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
-                       [                   0,                   0,            0,               1]])
-        T6_G = T6_G.subs(s)
-	
+    
 	# Now the calculations from baselink to all points:
 
         T0_2 = simplify(T0_1 * T1_2)
